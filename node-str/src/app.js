bad979e52c541cf848498ddbec3b8ad363d8d283
Ricardo Baniski <45471710+RicardoBaniski@users.noreply.github.com>
2019-10-09 18:17:48 -0300

    Criando APIs com NodeJs - Aula 14: Mongooose

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+//Conecta com o banco
+mongoose.connect('mongodb+srv://nodestr:nodestr@nodestr-nc0u7.mongodb.net/test?retryWrites=true&w=majority');
+
+//Carrehga as Rotas
 const indexRoute = require('./routes/index-rote');
 const productRoute = require('./routes/product-route');
 
